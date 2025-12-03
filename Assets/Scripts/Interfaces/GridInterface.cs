698466cfd7485e23fd3312fc6b0e57471b4f8e09
Adam Hopf <adamhopf@iastate.edu>
2025-12-03 13:31:45 -0600

    quick change to grid renderer to support search quieries

--- Assets/Scripts/Interfaces/GridInterface.cs
+++ Assets/Scripts/Interfaces/GridInterface.cs
@@ -1,13 +1,14 @@
 using System;
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public abstract class GridInterface : SingletonMonoBehaviour<GridInterface>
 {
     public abstract void MoveCreaturePosition(GameObject token, Vector3Int targetPosition, Vector3Int startPosition);
     public abstract Boolean IsCellWalkable(Vector3Int position);
-    public abstract IEnumerator targetSelect(int range, CoroutineResult<GameObject> result);
+    public abstract IEnumerator TargetSelect(int range, CoroutineResult<GameObject> result);
     public abstract void SetCreaturePosition(GameObject token, Vector3Int spawnPosition);
-
+    public abstract List<GameObject> GetOccupantsInArea(List<Vector3Int> area);
     
 }
