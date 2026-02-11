8318b23691cf4427350b6c3c7d86647f15585395
cboltjes <cbolt117@gmail.com>
2026-02-10 19:33:12 -0600

    Added StaticUnityEvents and added TurnStep to the Combat queue so Actions can be inserted to the turn order.

--- Assets/Scripts/Combat/CombatManager.cs
+++ Assets/Scripts/Combat/CombatManager.cs
@@ -1,14 +1,36 @@
 using UnityEngine;
 using System.Collections.Generic;
+using UnityEngine.Events;
 
 public class CombatManager : CombatManagerInterface
 {
-    protected Queue<ActionController> combatants = new();
+    protected List<ActionController> Combatants = new();
+    protected List<TurnStep> TurnQueue = new();
     protected ActionController TurnTaker = null;
 
     public override void AddCombatant(ActionController combatant)
     {
-        combatants.Enqueue(combatant);
+        Combatants.Add(combatant);
+        TurnQueue.Add(new TurnStep(combatant));
+    }
+    
+    public override void Remove(ActionController combatant)
+    {
+        Combatants.Remove(combatant);
+        for(int i = 0; i < TurnQueue.Count; i++) 
+        {
+            if (TurnQueue[i].Player == combatant)
+                TurnQueue.RemoveAt(i);
+        }
+    }
+
+    public override void Remove(UnityEvent e)
+    {
+        for (int i = 0; i < TurnQueue.Count; i++)
+        {
+            if (TurnQueue[i].Event == e)
+                TurnQueue.RemoveAt(i);
+        }
     }
 
     public override GameObject WhosTurn()
@@ -19,7 +41,7 @@ public class CombatManager : CombatManagerInterface
     public override List<GameObject> GetCombatants()
     {
         List<GameObject> list = new();
-        foreach(var c in combatants)
+        foreach (var c in Combatants)
             list.Add(c.gameObject);
         return list;
     }
@@ -34,19 +56,9 @@ public class CombatManager : CombatManagerInterface
 
     public override void NextTurn()
     {
-        ActionController e = combatants.Dequeue();
+        ActionController e = TurnQueue.Remove(0);
         TurnTaker = e;
         e.StartTurn();
-        combatants.Enqueue(e);
-    }
-
-    public override GameObject GetTarget(GameObject attacker)
-    {
-        foreach(var ac in combatants)
-        {
-            if (ac.GetInstanceID() != attacker.GetInstanceID())
-                return ac.gameObject;
-        }
-        return null;
+        combatants.Add(e);
     }
 }
