daaa715bc2d6ef2f3ae0fd89022e8a59368a578e
Ethan Griesman <egriesma@iastate.edu>
2026-02-12 15:31:03 -0600

    rough draft of walls code

--- Assets/Scripts/Grid/ImageToGrid.cs
+++ Assets/Scripts/Grid/ImageToGrid.cs
@@ -1,12 +1,15 @@
 using UnityEngine;
+using UnityEngine.U2D;
 //change to be non monoBehaviour, its not needed
 [System.Serializable]
 public class ImageToGrid
 {
     [Header("Level Image")]
     [SerializeField] private Texture2D img;
+    [SerializeField] private Texture3D img2;
 
     public int[,] grid;
+    public int[,,] wall;
 
     public int[,] GenerateGrid()
     {
@@ -23,9 +26,39 @@ public class ImageToGrid
         return grid;
     }
 
+    public int[,,] GenerateWall()
+    {
+        if (img2 == null) return null;
+        wall = new int[img2.width, img2.height, img2.depth];
+        for (int i = 0; i<img2.width; i++)
+        {
+            for (int j = 0; j < img2.height; j++)
+            {
+                for (int k = 0; k < img2.depth; k++)
+                {
+                    Color pixel = img2.GetPixel(i, j, k);
+                    wall[i, j, k] = (pixel == Color.white) ? 1 : 0;
+
+                }
+            }
+        }
+        return wall;
+    }
+
+
+
+    // seperate color to indicate tile_type.wall
+    // 3d cube prefab
+
+
     public int[,] GetGrid()
     {
         return grid;
+    }
+
+    public int[,,] GetWall()
+    {
+        return wall;
     }
 
     public int GetWidth()
@@ -35,6 +68,11 @@ public class ImageToGrid
     public int GetHeight()
     {
         return img.height;
+    }
+
+    public int GetDepth()
+    {
+        return img2.depth;
     }
 
     public void PrintGrid()
