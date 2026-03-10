fbfee2b010a9674fe473ca9e5b3c4a7d8e9fe30a
Ethan Griesman <egriesma@iastate.edu>
2026-03-10 14:57:16 -0500

    added fog prefab, fog of war still work in progress

--- BenchmarkSuite1/obj/Release/net9.0/BenchmarkSuite1.AssemblyInfo.cs
+++ BenchmarkSuite1/obj/Release/net9.0/BenchmarkSuite1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BenchmarkSuite1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d1764e14d8463fe88fd00561d53f5e6f072b643")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1621d35b6a594fc4bc67c8862f8985707fdd357a")]
 [assembly: System.Reflection.AssemblyProductAttribute("BenchmarkSuite1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BenchmarkSuite1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
