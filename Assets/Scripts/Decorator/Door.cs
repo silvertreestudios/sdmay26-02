39e342f29bdbda31589f00e1d27c94dfc619925e
Ethan Griesman <egriesma@iastate.edu>
2026-03-11 20:24:47 -0500

    beginning door hp

--- Assets/Scripts/Decorator/Door.cs
+++ Assets/Scripts/Decorator/Door.cs
@@ -1,22 +1,25 @@
 using UnityEngine;
+using System.Collections.Generic;
+using Game.Creature;
 
 public class Door : MonoBehaviour
 {
     public GameObject door;
+    public int maxHP = 1;
+    private int currentHP;
+
     IGridMemory gridMemory;
     int gridX;
     int gridZ;
     bool hasGridPosition;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        // door = gameObject;
+        currentHP = maxHP;
         gridMemory = IGridMemory.GetInstance();
         hasGridPosition = TryResolveGridPosition();
     }
 
-    // Update is called once per frame
     void Update()
     {
         if (door == null)
@@ -51,6 +54,69 @@ public class Door : MonoBehaviour
         SetDoorEnabled(!isOpen);
     }
 
+    // Called when door takes damage from an attack
+    public void TakeDamage(List<DamageValue> damageValues, D20Result attackRoll)
+    {
+        if (currentHP <= 0)
+        {
+            return; 
+        }
+
+        // Calculate total damage
+        int totalDamage = 0;
+        foreach (var damageValue in damageValues)
+        {
+            totalDamage += damageValue.DamageAmount;
+        }
+
+        currentHP -= totalDamage;
+        Debug.Log($"Door at ({gridX}, {gridZ}) took {totalDamage} damage. HP: {currentHP}/{maxHP}");
+
+        if (currentHP <= 0)
+        {
+            currentHP = 0;
+            OpenDoor();
+        }
+    }
+
+    // taking raw damage without attack roll
+    public void TakeDamage(int damage)
+    {
+        if (currentHP <= 0)
+        {
+            return;
+        }
+
+        currentHP -= damage;
+        Debug.Log($"Door at ({gridX}, {gridZ}) took {damage} damage. HP: {currentHP}/{maxHP}");
+
+        if (currentHP <= 0)
+        {
+            currentHP = 0;
+            OpenDoor();
+        }
+    }
+
+    private void OpenDoor()
+    {
+        if (!hasGridPosition || gridMemory == null)
+        {
+            return;
+        }
+
+        if (!gridMemory.IsDoor(gridX, gridZ))
+        {
+            return;
+        }
+
+        // Only open if currently closed
+        if (!gridMemory.IsDoorOpen(gridX, gridZ))
+        {
+            gridMemory.ToggleDoor(gridX, gridZ);
+            Debug.Log($"Door at ({gridX}, {gridZ}) has been broken open!");
+        }
+    }
+
     bool TryResolveGridPosition()
     {
         string[] nameParts = gameObject.name.Split('_');
@@ -74,5 +140,17 @@ public class Door : MonoBehaviour
     void SetDoorEnabled(bool enabled)
     {
         door.GetComponent<MeshRenderer>().enabled = enabled;
+    }
+
+    // Get the door's current HP for UI or targeting purposes
+    public int GetCurrentHP()
+    {
+        return currentHP;
+    }
+
+    // Check if door is broken (0 HP)
+    public bool IsBroken()
+    {
+        return currentHP <= 0;
     }
 }
