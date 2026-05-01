eae4c77027a226c2a27d8b027c3ef742af636bcd
adamhopf <adamhopf@iastate.edu>
2026-04-30 22:32:43 -0500

    added audio settings to settings menu and updated audio mixer to work with scripting

--- Assets/UIStuff/SettingsMenu/SettingsMenuControl.cs
+++ Assets/UIStuff/SettingsMenu/SettingsMenuControl.cs
@@ -1,16 +1,28 @@
 using System;
 using UnityEngine;
 using UnityEngine.UIElements;
+using UnityEngine.Audio;
 
 public class SettingsMenuControl : MonoBehaviour
 {
+    public AudioMixer audioMixer;
+
     public static event Action<float> OnLogOpacityChanged;
     public const string LogOpacityKey = "CombatLogOpacity";
     public const float LogOpacityDefault = 0.35f;
 
+    public const string MasterVolumeKey = "MasterVolume";
+    public const string MusicVolumeKey = "MusicVolume";
+    public const string SFXVolumeKey = "SFXVolume";
+    public const string FullscreenKey = "Fullscreen";
+
     public VisualElement ui;
     private Button backButton;
     private Slider logOpacitySlider;
+    private Slider masterVolumeSlider;
+    private Slider musicVolumeSlider;
+    private Slider sfxVolumeSlider;
+    private Toggle fullScreenToggle;
     private Action onCloseCallback;
 
     private void Awake()
@@ -29,6 +41,65 @@ public class SettingsMenuControl : MonoBehaviour
         {
             logOpacitySlider.value = PlayerPrefs.GetFloat(LogOpacityKey, LogOpacityDefault);
             logOpacitySlider.RegisterValueChangedCallback(OnOpacitySliderChanged);
+        }
+
+        masterVolumeSlider = ui.Q<Slider>("MasterVolumeSlider");
+        if (masterVolumeSlider != null)
+        {
+            masterVolumeSlider.value = PlayerPrefs.GetFloat(MasterVolumeKey, 0.5f);
+            SetVolume("MasterVolume", masterVolumeSlider.value);
+            masterVolumeSlider.RegisterValueChangedCallback(evt => 
+            {
+                PlayerPrefs.SetFloat(MasterVolumeKey, evt.newValue);
+                SetVolume("MasterVolume", evt.newValue);
+            });
+        }
+
+        musicVolumeSlider = ui.Q<Slider>("MusicVolumeSlider");
+        if (musicVolumeSlider != null)
+        {
+            musicVolumeSlider.value = PlayerPrefs.GetFloat(MusicVolumeKey, 0.5f);
+            SetVolume("MusicVolume", musicVolumeSlider.value);
+            musicVolumeSlider.RegisterValueChangedCallback(evt => 
+            {
+                PlayerPrefs.SetFloat(MusicVolumeKey, evt.newValue);
+                SetVolume("MusicVolume", evt.newValue);
+            });
+        }
+
+        sfxVolumeSlider = ui.Q<Slider>("SFXVolumeSlider");
+        if (sfxVolumeSlider != null)
+        {
+            sfxVolumeSlider.value = PlayerPrefs.GetFloat(SFXVolumeKey, 0.5f);
+            SetVolume("SFXVolume", sfxVolumeSlider.value);
+            sfxVolumeSlider.RegisterValueChangedCallback(evt => 
+            {
+                PlayerPrefs.SetFloat(SFXVolumeKey, evt.newValue);
+                SetVolume("SFXVolume", evt.newValue);
+            });
+        }
+
+        fullScreenToggle = ui.Q<Toggle>();
+        if (fullScreenToggle != null)
+        {
+            bool isFullscreen = PlayerPrefs.GetInt(FullscreenKey, 1) == 1;
+            fullScreenToggle.value = isFullscreen;
+            Screen.fullScreen = isFullscreen;
+            fullScreenToggle.RegisterValueChangedCallback(evt =>
+            {
+                PlayerPrefs.SetInt(FullscreenKey, evt.newValue ? 1 : 0);
+                Screen.fullScreen = evt.newValue;
+            });
+        }
+    }
+
+    private void SetVolume(string parameterName, float sliderValue)
+    {
+        if (audioMixer != null)
+        {
+            // Convert linear slider (0-1) to logarithmic dB (-80 to 0)
+            float dbValue = sliderValue > 0.0001f ? Mathf.Log10(sliderValue) * 20f : -80f;
+            audioMixer.SetFloat(parameterName, dbValue);
         }
     }
 
